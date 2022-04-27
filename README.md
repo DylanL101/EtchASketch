50acc5cab80dc9861ea045662cd6b84c84aea0ae
Dylan <dylblob@gmail.com>
2022-04-27 16:54:14 -0400

    Add a mouseover function That changes the color of each individual box when you hover of it, using addEventListener.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EtchASketch
+Hi all, 
+
+This is the Etch A Sketch project as part of The Odin Project. I hope this a great learning experience  for me!
--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 const contain = document.getElementById("contain");
 
 function createRows( rows, columns) {
-    contain.style.setProperty('-grid-rows', rows);
-    contain.style.setProperty('-grid-cols', columns);
+    contain.style.setProperty('--grid-rows', rows);
+    contain.style.setProperty('--grid-cols', columns);
     for (i = 0; i < (rows*columns); i++) {
         let box= document.createElement('div');
         box.innerText = (i + 1);
         contain.appendChild(box).className = "gridBox";
+        box.addEventListener( 'mouseover', () => {
+            box.style.backgroundColor = 'blue';
+            
+        })
     };
 };
 
