1d1aec979905ce2fb1c888f6c0b55c05ed779efe
Dylan <dylblob@gmail.com>
2022-04-27 21:53:25 -0400

    Add several style properties to the js file Ensure that there is no overflow, that the contain element has a width of 960px, and that the box properties have no overflow and respective height and width properties.

--- sketch.js
+++ sketch.js
@@ -1,17 +1,34 @@
 const contain = document.getElementById("contain");
+let click =true;
 
 function createRows( rows, columns) {
     contain.style.setProperty('--grid-rows', rows);
     contain.style.setProperty('--grid-cols', columns);
+    contain.style.overflow = 'hidden';
+    contain.style.width = '960px';
     for (i = 0; i < (rows*columns); i++) {
         let box= document.createElement('div');
-        box.innerText = (i + 1);
+        box.style.minWidth ='0';
+        box.style.minHeight = '0';
+        boz.style.overflow = 'hidden';
+        // box.innerText = (i + 1);
         contain.appendChild(box).className = "gridBox";
         box.addEventListener( 'mouseover', () => {
-            box.style.backgroundColor = 'blue';
-            
+            box.style.backgroundColor = 'red';
+
         })
     };
 };
 
 createRows( 16, 16);
+ //const button = document.createElement('BUTTON');
+//contain.append(button);
+//button.id = "RESTART";
+//button.addEventListener('click', () => {
+
+
+})
+
+//document.querySelector('body').addEventListener('click', ()=>{
+    click =!click
+})
