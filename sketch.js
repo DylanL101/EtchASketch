a7743f7f41a1f36645e8727cbc5444a1b76e8b9f
Dylan <dylblob@gmail.com>
2022-04-27 22:48:42 -0400

    Create a restart button This button should restart the game and ask the user for a size between 2 and which represents the dimensions of the grid.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const contain = document.getElementById("contain");
-let click =true;
+//let click =true;
 
-function createRows( rows, columns) {
+function createGrid( rows, columns) {
     contain.style.setProperty('--grid-rows', rows);
     contain.style.setProperty('--grid-cols', columns);
     contain.style.overflow = 'hidden';
@@ -10,7 +10,7 @@ function createRows( rows, columns) {
         let box= document.createElement('div');
         box.style.minWidth ='0';
         box.style.minHeight = '0';
-        boz.style.overflow = 'hidden';
+        box.style.overflow = 'hidden';
         // box.innerText = (i + 1);
         contain.appendChild(box).className = "gridBox";
         box.addEventListener( 'mouseover', () => {
@@ -18,17 +18,36 @@ function createRows( rows, columns) {
 
         })
     };
+    userButton()
 };
+function userButton(){
+    const btn = document.querySelector('#btn');
+    const restart = document.createElement('button');
+    restart.textContent ="Restart Game!";
+    restart.style.padding = "10px";
+    restart.style.margin = "20px";
+    btn.appendChild(restart);
+    restart.addEventListener('click', () => {
+        document.querySelectorAll('.gridBox').forEach(e => e.remove());
+        let input = prompt('select a grid size from 2 to 100');
+        if ( input < 2 && input >100){
+            return "ERROR: insert a number between 2 and 100!";
+        }
+        rows = input; 
+        columns = input;
+        createGrid(rows, columns);
+    })
 
-createRows( 16, 16);
+
+}
+createGrid( 16, 16);
  //const button = document.createElement('BUTTON');
 //contain.append(button);
 //button.id = "RESTART";
 //button.addEventListener('click', () => {
 
 
-})
+//})
 
 //document.querySelector('body').addEventListener('click', ()=>{
-    click =!click
-})
+   
